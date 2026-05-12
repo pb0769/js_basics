fd0f6150debe44e483367545f698e319fba734e0
pb0769 <parthbansal0769@gmail.com>
2026-05-12 18:52:35 +0530

    variables
    Added variable declarations and console logs for demonstration.

---
+++ index.js
@@ -0,0 +1,17 @@
+//----Variables----
+ console.log('Fuck You');
+ let name = 'Parth';            //let is used to define a variable 
+ console.log(name);
+
+ // ---- Rules for Variable names----
+ // Cannot be a reserved keyword
+ // Should be meaningful
+ // Cannot start with a number (1name)
+ // Cannot contain a space or hyphen (-)
+ // Are case-sensitive
+
+ let firstName = 'Parth' , lastName = 'Bansal';
+ console.log(firstName , lastName);
+
+ let age = 18;
+ console.log(age);
